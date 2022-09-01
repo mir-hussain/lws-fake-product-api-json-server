3cf39d25463985b96559e547c37f937f146a3c64
mir-hussain <mirhussainmurtaza@gmail.com>
2022-09-01 22:27:54 +0600

    missing data issue solved

--- db.js
+++ db.js
@@ -2,11 +2,14 @@ const todos = require("./data/todos");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
+const tags = require("./data/tags");
+const videoCount = require("./data/video-count");
 
 module.exports = {
   todos,
   videos,
   videos2,
   transactions,
-}
-
+  tags,
+  videoCount,
+};
